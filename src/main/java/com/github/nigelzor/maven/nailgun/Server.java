7f4b059c7b0bbeccddaf2ec8b945356ce3d91613
Neil Gentleman <ngentleman@gmail.com>
2013-02-26 22:18:01 -0800

    my localhost isn't the same as your localhost
    (but binding to all interfaces by default is pretty poor)

--- src/main/java/com/github/nigelzor/maven/nailgun/Server.java
+++ src/main/java/com/github/nigelzor/maven/nailgun/Server.java
@@ -12,14 +12,35 @@ public class Server {
 	public static void main(String[] args, ClassWorld classWorld) throws Exception {
 		// bah, this should just be a call to NGServer.main(), but plexus doesn't want us to just return
 		if (args.length > 1) {
-			System.err.println("only expecting one arg: port");
+			System.err.println("only expecting one arg: [host:]port");
 			return;
 		}
 
-		InetAddress serverAddress = InetAddress.getLocalHost();
+		InetAddress serverAddress = null;
 		int port = NGConstants.DEFAULT_PORT;
+
+		// parse the sole command line parameter, which
+		// may be an inetaddress to bind to, a port number,
+		// or an inetaddress followed by a port, separated
+		// by a colon
 		if (args.length != 0) {
-			port = Integer.parseInt(args[0]);
+			String[] argParts = args[0].split(":");
+			String addrPart = null;
+			String portPart = null;
+			if (argParts.length == 2) {
+				addrPart = argParts[0];
+				portPart = argParts[1];
+			} else if (argParts[0].indexOf('.') >= 0) {
+				addrPart = argParts[0];
+			} else {
+				portPart = argParts[0];
+			}
+			if (addrPart != null) {
+				serverAddress = InetAddress.getByName(addrPart);
+			}
+			if (portPart != null) {
+				port = Integer.parseInt(portPart);
+			}
 		}
 
 		NGServer server = new NGServer(serverAddress, port, NGServer.DEFAULT_SESSIONPOOLSIZE);
